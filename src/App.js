6f576eebe2c270040066a7dca67ae06c82cb1462
Hemant Bhoi <56757949+hkbcode@users.noreply.github.com>
2021-09-14 10:01:21 +0530

    :fire:

---
+++ src/App.js
@@ -0,0 +1,15 @@
+//IMPORTANT : Edit package.json before packaging app for production
+
+import { Button } from 'antd'
+
+function App() {
+  return (
+    <div className="App" style={{marginLeft: 100}}>
+      <h1> Electron with Reactjs and</h1>    
+     <Button type="primary"> Ant Design</Button>
+      <p> Start Building Desktop Apps with ElectronJs.</p>
+    </div>
+  );
+}
+
+export default App;
